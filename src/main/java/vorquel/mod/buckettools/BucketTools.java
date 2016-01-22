0fa4c1c9c0602cb29f3ae4dc262217c7645ff1b4
Vorquel <vorquel@gmail.com>
2016-01-22 12:07:30 -0700

    Update to 1.8.9

--- src/main/java/vorquel/mod/buckettools/BucketTools.java
+++ src/main/java/vorquel/mod/buckettools/BucketTools.java
@@ -1,39 +1,31 @@
 package vorquel.mod.buckettools;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.relauncher.Side;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import vorquel.mod.buckettools.helper.Ref;
 import vorquel.mod.buckettools.proxy.Proxy;
 
-@Mod(modid = Ref.MOD_ID, name = "Bucket Tools", version = "@MOD_VERSION@")
+@SuppressWarnings("unused")
+@Mod(modid = Ref.MOD_ID, name = "Bucket Tools", version = "@VERSION@")
 public class BucketTools {
-
-    @Mod.Instance(Ref.MOD_ID)
-    @SuppressWarnings("unused")
+    
+    @Instance(Ref.MOD_ID)
     public static BucketTools instance;
-
+    
     @SidedProxy(serverSide = "vorquel.mod.buckettools.proxy.Proxy", clientSide = "vorquel.mod.buckettools.proxy.ProxyClient")
     public static Proxy proxy;
-
-    @Mod.EventHandler
-    @SuppressWarnings("unused")
-    public void preInit(FMLPreInitializationEvent event) {
-        proxy.preInit(event);
-    }
-
-    @Mod.EventHandler
-    @SuppressWarnings("unused")
-    public void init(FMLInitializationEvent event) {
-        proxy.init();
-    }
-
-    @Mod.EventHandler
-    @SuppressWarnings("unused")
-    public void postInit(FMLPostInitializationEvent event) {
-        proxy.postInit();
-    }
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {}
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event) {}
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {}
 }
